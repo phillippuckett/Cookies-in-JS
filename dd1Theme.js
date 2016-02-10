7465d7701da0eea42ddeafc5dda1027a2d9b015e
Phillip Puckett <phillippuckett88@gmail.com>
2016-02-10 11:39:41 -0700

    cookie added

--- dd1Theme.js
+++ dd1Theme.js
@@ -2,14 +2,14 @@ window.onload = function () {
     if (document.cookie.length != 0) {
         var nameValueArray = document.cookie.split('=');
         document.bgColor = nameValueArray[1];
-        document.getElementById('colors').value = nameValueArray[1];
+        document.getElementById('dd1Theme').value = nameValueArray[1];
     }
 };
 
 var setColorCookie = function () {
-    var selectedColor = document.getElementById('colors').value;
+    var selectedColor = document.getElementById('dd1Theme').value;
     if (selectedColor != 'Select Color') {
-        document.bgColor = selectedColor
+        document.bgColor = selectedColor;
         document.cookie = 'color=' + selectedColor + ';expires=Fri, 5 Aug 2016 01:00:00 UTC;';
     }
 };
