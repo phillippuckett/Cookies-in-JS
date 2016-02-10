ca816d544e092329dd45a28391bb7ec1fa59d135
Phillip Puckett <phillippuckett88@gmail.com>
2016-02-10 11:26:23 -0700

    cookie added

--- colors.js
+++ colors.js
@@ -1,13 +1,14 @@
-var setColor = function () {
+window.onload = function () {
+    if (document.cookie.length != 0) {
+        var nameValueArray = document.cookie.split('=');
+        document.bgColor = nameValueArray[1];
+        document.getElementById('colors').value = nameValueArray[1];
+    }
+};
+
+var setColorCookie = function () {
     var selectedColor = document.getElementById('colors').value;
     if (selectedColor != 'Select Color') {
-        window. = function () {
-            if (document.cookie.length != 0) {
-                var nameValueArray = document.cookie.split('=');
-                document.bgColor = nameValueArray[1];
-                document.getElementById('colors').value = nameValueArray[1];
-            }
-        };
         document.bgColor = selectedColor
         document.cookie = 'color=' + selectedColor + ';expires=Friday, 5 Aug 2016 01:00:00 UTC;';
     }
