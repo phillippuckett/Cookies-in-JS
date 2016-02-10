209dc1fe325292000cc419344222bcd199042839
Phillip Puckett <phillippuckett88@gmail.com>
2016-02-10 11:23:08 -0700

    cookie added

--- colors.js
+++ colors.js
@@ -1,6 +1,14 @@
 var setColor = function () {
     var selectedColor = document.getElementById('colors').value;
     if (selectedColor != 'Select Color') {
-        document.bgColor = selectedColor;
+        window. = function () {
+            if (document.cookie.length != 0) {
+                var nameValueArray = document.cookie.split('=');
+                document.bgColor = nameValueArray[1];
+                document.getElementById('colors').value = nameValueArray[1];
+            }
+        };
+        document.bgColor = selectedColor
+        document.cookie = 'color=' + selectedColor + ';expires=Friday, 5 Aug 2016 01:00:00 UTC;';
     }
 };
